900f0630193caf14e7021bb3501118cc1c40a4f1
Filip Jaredson <filja057@student.liu.se>
2021-02-04 14:12:13 +0100

    Multi layer backpropogation implemented

--- Assignments/A1_Supervised/multiLayerNetwork/runMultiLayer.m
+++ Assignments/A1_Supervised/multiLayerNetwork/runMultiLayer.m
@@ -12,9 +12,9 @@ function [ Y, L, U ] = runMultiLayer( X, W, V )
 %              U - Activation of hidden neurons (vector)
 
 % Add your own code here
-S = 0; % Calculate the weighted sum of input signals (hidden neuron)
-U = 0; % Calculate the activation of the hidden neurons (use hyperbolic tangent)
-Y = 0; % Calculate the weighted sum of the hidden neurons
+S = X * W; % Calculate the weighted sum of input signals (hidden neuron)
+U = tanh(S); % Calculate the activation of the hidden neurons (use hyperbolic tangent)
+Y = U * V; % Calculate the weighted sum of the hidden neurons
 
 % Calculate labels
 [~, L] = max(Y,[],2);
